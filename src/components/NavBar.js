88899310271ed1d2c395e3ea80dc67bee71a028d
ClarabelleCheng-Yue <cachengyue@gmail.com>
2018-02-25 14:43:43 -0800

    Add styled components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 import Home from "./Home";
 import About from "./About";
@@ -10,15 +10,17 @@ import Contact from "./Contact";
 export default class NavBar extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route path="/products" component={Products} />
-          <Route path="/careers" component={Careers} />
-          <Route path="/contact" component={Contact} />
-        </Switch>
-      </BrowserRouter>
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/products" component={Products} />
+            <Route exact path="/careers" component={Careers} />
+            <Route exact path="/contact" component={Contact} />
+          </Switch>
+        </BrowserRouter>
+      </ div>
     );
   }
 }
---
+++ src/components/styles/common.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const NavBarStyle = styled.ul `
+  list-style-type: none;
+  li {
+    position: relative;
+    display: inline-block;
+    margin-right: 1rem;
+  }
+`;
